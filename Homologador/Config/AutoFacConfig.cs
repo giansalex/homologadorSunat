8da62c197938d9ab9031a6b5b531774e1833ee09
Giancarlos Salas <giansalex@gmail.com>
2018-02-24 22:33:53 -0500

    add autofac config

---
+++ Homologador/Config/AutoFacConfig.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using MetroFramework.Forms;
+using System.Linq;
+using System.Reflection;
+
+namespace Homologador.Config
+{
+    public static class AutoFacConfig
+    {
+        public static IContainer BootStrap()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<StudentRepository>()
+                .As<IModelRepository<WindowsApp.Models.Student>>()
+                .InstancePerDependency();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly)
+                .Where(type => type.IsSubclassOf(typeof(MetroForm)));
+
+            return builder.Build();
+        }
+    }
+}
